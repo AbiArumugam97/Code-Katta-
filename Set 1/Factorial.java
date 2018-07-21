5c74e84d70e7eed9df74e363ef63e7d28e074223
AbiArumugam97 <41355410+AbiArumugam97@users.noreply.github.com>
2018-07-21 22:30:16 +0530

    Update Factorial.java

--- Set 1/Factorial.java
+++ Set 1/Factorial.java
@@ -1,26 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package fact;
 
 import java.util.Scanner;
+
+/**
+ *
+ * @author Abi
+ */
 public class Fact {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        // TODO code application logic here
         Scanner s=new Scanner(System.in);
         String ip=s.next();
         int i,fact=1;
         try
         {
-          int a=Integer.parseInt(ip);  
-            if(a==0)
+          int a=Integer.parseInt(ip);
+          if(a<=20)
+          {
+          if(a==0)
           {
               System.out.println("1");
           }
-            else
-            {
+          else
+          {
          for(i=1;i<=a;i++)
         {
             fact*=i;
         }
-        System.out.println("Factorial of "+a+" is "+fact);
+        System.out.println(+fact);
     }
+        }
+          else
+          {
+              System.out.println("Range Exceeds");
+          }
         }
         catch(NumberFormatException e)
         {
