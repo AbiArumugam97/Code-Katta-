c40917a44bbf4a7eb03e10f9549b1262cc5e4c03
AbiArumugam97 <41355410+AbiArumugam97@users.noreply.github.com>
2018-07-21 23:16:28 +0530

    Update String reverse.java

--- Set 1/String reverse.java
+++ Set 1/String reverse.java
@@ -1,30 +1,21 @@
 package string.reverse;
+import java.util.Scanner;
 public class StringReverse {
     public static void main(String[] args) {
-        String s="111222";
-        try
-        {
-        if(s.length()<=100000)
-        {
-        if(s.isEmpty())
+        Scanner ip=new Scanner(System.in);
+        String s=ip.nextLine();
+        if(s.length()==0)
         {
             System.out.println("String is null");
         }
-        else
+        else if(s.length()>0 && s.length()<=100000)
         {
         StringBuilder sb=new StringBuilder(s);
-        System.out.println(s);
         System.out.println(sb.reverse());
-        }
         }
         else
         {
             System.out.println("Error");
         }
-    }
-    catch(NumberFormatException e)
-    {
-        System.out.println("Exception");
-    }
 }
 }
